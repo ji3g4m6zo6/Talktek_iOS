a783df45c873cf8ee70d0912a2a008786e2b142a
Sunny <sunnylee945@hotmail.com>
2017-11-18 18:30:02 +0800

    Underline Segmented Control Extension

--- talkTek/Controller/HomeViewController.swift
+++ talkTek/Controller/HomeViewController.swift
@@ -9,27 +9,37 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationController?.navigationBar.prefersLargeTitles = true
+    searchImplement()
+    segmentedControl.addUnderlineForSelectedSegment()
+    segmentedControl.tintColor = UIColor.tealish()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func searchImplement(){
+    let search = UISearchController(searchResultsController: nil)
+    search.searchResultsUpdater = self as? UISearchResultsUpdating
+    self.navigationItem.searchController = search
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
