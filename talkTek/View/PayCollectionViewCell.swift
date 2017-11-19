8933453e1451e01237129c151b5f419ea03b7a17
Sunny <sunnylee945@hotmail.com>
2017-11-19 16:12:46 +0800

    UIImageViewExtension for async image download

---
+++ talkTek/View/PayCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PayCollectionViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/19.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class PayCollectionViewCell: UICollectionViewCell {
+    
+}
