dc0f4c2b166658d1207d07b2a59e41945f00bf49
Sunny <sunnylee945@hotmail.com>
2017-11-21 15:45:55 +0800

    HomeVC: Add the two VC of HomeVC, PayCourseVC and FreeCourseVC. Initially added

--- talkTek/Controller/HomeViewController.swift
+++ talkTek/Controller/HomeViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
+  lazy var payViewController: PayCourseViewController = {
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    var viewController = storyboard.instantiateViewController(withIdentifier: "PayCourseViewController") as! PayCourseViewController
+    self.addViewControllerAsChildViewController(childViewController: viewController)
+    return viewController
+  }()
+  lazy var freeViewController: FreeCourseViewController = {
+    let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+    var viewController = storyboard.instantiateViewController(withIdentifier: "FreeCourseViewController") as! FreeCourseViewController
+    self.addViewControllerAsChildViewController(childViewController: viewController)
+    return viewController
+  }()
+  
   
   @IBOutlet weak var segmentedControl: UISegmentedControl!
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationController?.navigationBar.prefersLargeTitles = true
     searchImplement()
+    
+    setUpView()
     segmentedControl.addUnderlineForSelectedSegment()
-
+    
     
   }
   
@@ -32,10 +47,55 @@ class HomeViewController: UIViewController {
     self.navigationItem.searchController = search
   }
   
+
   
-  @IBAction func segmentedControlDidChange(_ sender: UISegmentedControl) {
+  private func setUpView() {
+    setUpSegmentedControl()
+    
+    updateView()
+  }
+  
+  private func updateView() {
+    payViewController.view.isHidden = !(segmentedControl.selectedSegmentIndex == 0)
+    freeViewController.view.isHidden = (segmentedControl.selectedSegmentIndex == 0)
+  }
+  
+  private func setUpSegmentedControl() {
+    
+    
+    segmentedControl.removeAllSegments()
+    segmentedControl.insertSegment(withTitle: "課程小講", at: 0, animated: false)
+    segmentedControl.insertSegment(withTitle: "免費膠囊", at: 1, animated: false)
+    segmentedControl.addTarget(self, action: #selector(selectionDidChange(sender:)), for: .valueChanged)
+    
+    segmentedControl.selectedSegmentIndex = 0
+    
+  }
+  
+  @objc func selectionDidChange(sender: UISegmentedControl) {
     segmentedControl.changeUnderlinePosition()
     
+    updateView()
+  }
+  
+  private func addViewControllerAsChildViewController(childViewController: UIViewController) {
+    addChildViewController(childViewController)
+    
+    view.insertSubview(childViewController.view, at: 0)
+    
+    childViewController.view.frame = view.bounds
+    childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+    childViewController.didMove(toParentViewController: self)
+    
+  }
+  
+  private func removeViewControllerAsChildViewController(childViewController: UIViewController) {
+    childViewController.willMove(toParentViewController: nil)
+    
+    childViewController.view.removeFromSuperview()
+    
+    childViewController.removeFromParentViewController()
   }
   /*
    // MARK: - Navigation
@@ -48,3 +108,4 @@ class HomeViewController: UIViewController {
    */
   
 }
+
