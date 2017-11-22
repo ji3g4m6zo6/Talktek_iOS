a849a44a5788eba9ff4473d1ed350457cb3319b9
Sunny <sunnylee945@hotmail.com>
2017-11-23 00:11:21 +0800

    Add all tableview cell files for CourseDetailVC and add the MainTBViewCell Outlets

---
+++ talkTek/View/CourseInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseInfoTableViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/23.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class CourseInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ talkTek/View/TeacherInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeacherInfoTableViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/23.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class TeacherInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
