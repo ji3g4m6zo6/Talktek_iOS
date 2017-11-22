7588ba29b610995aae449698cc0280ef46ae795f
Sunny <sunnylee945@hotmail.com>
2017-11-22 19:13:09 +0800

    Add CourseDetailVC file and main storyboard

---
+++ talkTek/Controller/CourseDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CourseDetailViewController.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/21.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class CourseDetailViewController: UIViewController {
+  
+  enum DetailViewSection: Int{
+    case main = 0
+    case courseInfo = 1
+    case teacherInfo = 2
+    case courses = 3
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  
+}
