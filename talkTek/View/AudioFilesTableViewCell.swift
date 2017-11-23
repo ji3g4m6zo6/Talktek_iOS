d2e71b3ab182acc588859e1cbd6486afb9847882
Sunny <sunnylee945@hotmail.com>
2017-11-23 22:35:01 +0800

    Add AudioFilesTableViewCell

---
+++ talkTek/View/AudioFilesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AudioFilesTableViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/23.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class AudioFilesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- talkTek/View/CoursesTableViewCell.swift
+++ talkTek/View/CoursesTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class CoursesTableViewCell: UITableViewCell {
-
+  var categories = ["標題一", "標題二"]
+  
   @IBOutlet weak var course_TableView: UITableView!
   override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,4 +23,21 @@ class CoursesTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+}
+extension CoursesTableViewCell: UITableViewDataSource, UITableViewDelegate{
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return categories.count
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CoursesHeartTableViewCell
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 109
+  }
 }
