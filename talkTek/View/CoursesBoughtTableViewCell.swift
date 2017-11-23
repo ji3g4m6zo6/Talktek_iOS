d46c09fda3b970e1d119cbbf38907bb044c6e40e
Sunny <sunnylee945@hotmail.com>
2017-11-23 21:21:18 +0800

    CoursesVC and tableviews and autolayouts

---
+++ talkTek/View/CoursesBoughtTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoursesBoughtTableViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/23.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class CoursesBoughtTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
