7dfbb7d50a82b552e682b636c342eb08f2d40349
Sunny <sunnylee945@hotmail.com>
2017-11-23 21:43:29 +0800

    Add CoursesBought, Heart TableView Cell outlets

--- talkTek/View/CoursesHeartTableViewCell.swift
+++ talkTek/View/CoursesHeartTableViewCell.swift
@@ -9,16 +9,24 @@
 import UIKit
 
 class CoursesHeartTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+  @IBOutlet weak var overview_ImageView: UIImageView!
+  
+  @IBOutlet weak var title_Label: UILabel!
+  
+  @IBOutlet weak var teacherName_Label: UILabel!
+  
+  @IBOutlet weak var studentNumber_Label: UILabel!
+  
+  @IBOutlet weak var giveStarNumber_Label: UILabel!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
 }
