9c6993ab63f3cf94654246614b474bd4006df7a0
Sunny <sunnylee945@hotmail.com>
2017-11-23 19:11:37 +0800

    CourseDetailVC tableview datasource and delegate

--- talkTek/Controller/CourseDetailViewController.swift
+++ talkTek/Controller/CourseDetailViewController.swift
@@ -20,8 +20,8 @@ class CourseDetailViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
+    tableView.delegate = self
+    tableView.dataSource = self
   }
   
   override func didReceiveMemoryWarning() {
@@ -31,4 +31,60 @@ class CourseDetailViewController: UIViewController {
   
   
   
+}
+extension CourseDetailViewController: UITableViewDelegate, UITableViewDataSource{
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 4
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    switch section {
+    case DetailViewSection.main.rawValue:
+      return 1
+    case DetailViewSection.courseInfo.rawValue:
+      return 1
+    case DetailViewSection.teacherInfo.rawValue:
+      return 1
+    case DetailViewSection.courses.rawValue:
+      return 1
+    default: fatalError()
+    }
+    
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.section {
+    case DetailViewSection.main.rawValue:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "main", for: indexPath) as! MainTableViewCell
+      
+      
+      return cell
+    case DetailViewSection.courseInfo.rawValue:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "courseInfo", for: indexPath) as! CourseInfoTableViewCell
+     
+      
+      return cell
+    case DetailViewSection.teacherInfo.rawValue:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "teacherInfo", for: indexPath) as! TeacherInfoTableViewCell
+      
+      return cell
+    case DetailViewSection.courses.rawValue:
+      let cell = tableView.dequeueReusableCell(withIdentifier: "courses", for: indexPath) as! CoursesTableViewCell
+      
+      return cell
+    default: fatalError()
+    }
+  }
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    switch indexPath.section {
+    case DetailViewSection.main.rawValue:
+      return 347.0
+    case DetailViewSection.courseInfo.rawValue:
+      return 190.0
+    case DetailViewSection.teacherInfo.rawValue:
+      return 239.0
+    case DetailViewSection.courses.rawValue:
+      return 546.0
+    default:
+      fatalError()
+    }
+  }
 }
