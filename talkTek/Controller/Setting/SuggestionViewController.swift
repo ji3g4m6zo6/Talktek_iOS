5fe8f346a13e16d7593285a174dcfb350bf54a08
Sunny <sunnylee945@hotmail.com>
2017-11-28 22:07:23 +0800

    Setting ViewControllers swift file

---
+++ talkTek/Controller/Setting/SuggestionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SuggestionViewController.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/28.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  
+}
