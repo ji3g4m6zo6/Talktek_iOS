b42e8e1e0b0d2dee9a6a55c5ce8d1207c87904de
Sunny <sunnylee945@hotmail.com>
2017-11-28 22:55:05 +0800

    Restructure the Xcode

---
+++ talkTek/View/SettingMainTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SettingMainTableViewCell.swift
+//  talkTek
+//
+//  Created by Mac on 2017/11/28.
+//  Copyright © 2017年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingMainTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var settingTitle_Label: UILabel!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
