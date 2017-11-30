d6a2c0ad37ded46f113837da5df6cf713b871fc6
Sunny <sunnylee945@hotmail.com>
2017-11-30 21:24:55 +0800

    Add FB log in set up

--- talkTek/Supporting File/AppDelegate.swift
+++ talkTek/Supporting File/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import Firebase
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
+    
+    FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    
     return true
+  }
+  
+  func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+    
+    return handled
   }
 
   func applicationWillResignActive(_ application: UIApplication) {
