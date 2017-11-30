7cb5af43851b43bf1ac0de329b318ec0fcb0199f
Sunny <sunnylee945@hotmail.com>
2017-11-30 21:25:08 +0800

    Setting for tableview didselect

--- talkTek/Controller/Setting/SettingViewController.swift
+++ talkTek/Controller/Setting/SettingViewController.swift
@@ -44,6 +44,23 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource
     cell.settingTitle_Label.text = list[indexPath.row]
     return cell
   }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.row == 0{
+      performSegue(withIdentifier: "points", sender: self)
+    } else if indexPath.row == 1{
+      performSegue(withIdentifier: "lecturer", sender: self)
+    } else if indexPath.row == 2{
+      performSegue(withIdentifier: "suggestion", sender: self)
+    } else if indexPath.row == 3{
+      performSegue(withIdentifier: "personal", sender: self)
+    } else if indexPath.row == 4{
+      performSegue(withIdentifier: "coupon", sender: self)
+    } else if indexPath.row == 5{
+      performSegue(withIdentifier: "about", sender: self)
+    } else{
+      print("WTF happened!!!")
+    }
+  }
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
     return 40
   }
