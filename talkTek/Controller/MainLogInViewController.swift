c33077e0f5d30671bb45be83cd4061f63f46facd
Sunny <sunnylee945@hotmail.com>
2017-11-30 22:56:51 +0800

    facebook successfully tested

--- talkTek/Controller/MainLogInViewController.swift
+++ talkTek/Controller/MainLogInViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
 
 class MainLogInViewController: UIViewController {
   
@@ -21,15 +23,44 @@ class MainLogInViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  @IBAction func facebookLogin(sender: UIButton) {
+    let fbLoginManager = FBSDKLoginManager()
+    fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+      if let error = error {
+        print("Failed to login: \(error.localizedDescription)")
+        return
+      }
+      
+      guard let accessToken = FBSDKAccessToken.current() else {
+        print("Failed to get access token")
+        return
+      }
+      
+      let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+      
+      // Perform login by calling Firebase APIs
+      Auth.auth().signIn(with: credential, completion: { (user, error) in
+        if let error = error {
+          print("Login error: \(error.localizedDescription)")
+          let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+          let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+          alertController.addAction(okayAction)
+          self.present(alertController, animated: true, completion: nil)
+          
+          return
+        }
+        
+        // Present the main view
+        if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+          UIApplication.shared.keyWindow?.rootViewController = viewController
+          self.dismiss(animated: true, completion: nil)
+        }
+        
+      })
+      
+    }
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  
   
 }
