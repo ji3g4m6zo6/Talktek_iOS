cd08899e8088e3b0064c26f3748370c1d254e979
Sunny <sunnylee945@hotmail.com>
2018-01-29 22:29:51 +0800

    Setting add gesture to image view

--- talkTek/Controller/Setting/SettingViewController.swift
+++ talkTek/Controller/Setting/SettingViewController.swift
@@ -15,44 +15,58 @@ import FBSDKLoginKit
 
 class SettingViewController: UIViewController {
   
+  // MARK: Outlets
   @IBOutlet weak var profile_ImageView: UIImageView!
-  
-  @IBOutlet weak var name_Label: UILabel!
-  
-  var databaseRef: DatabaseReference!
-  var username = ""
-  var userID = ""
-  var list = ["成為講師", "意見反饋", "優惠碼", "關於"]
-  @IBOutlet weak var tableView: UITableView!
-  
   @IBOutlet weak var cameraIcon_Button: UIButton!
-  
+  @IBOutlet weak var name_Label: UILabel!
   @IBOutlet weak var LogIn_LogOut: UIButton!
   
+  @IBOutlet weak var Edit_Button: UIBarButtonItem!
+  @IBOutlet weak var tableView: UITableView!
+  var list = ["成為講師", "意見反饋", "優惠碼", "關於"]
   
+  var username = ""
+  var userID = ""
+  
+  // MARK: Database
+  var databaseRef: DatabaseReference!
+  var user = User()
+
+  // MARK: viewDidLoad, didReceiveMemoryWarning, viewWillAppear
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // TableView delegate, datasource
     tableView.delegate = self
     tableView.dataSource = self
     
     Edit_Button.width = 0
     
+    // Circle Image
     cameraIcon_Button.tintColor = UIColor.audioPlayGray()
-   cameraIcon_Button.layer.cornerRadius = cameraIcon_Button.bounds.height / 2
+    cameraIcon_Button.layer.cornerRadius = cameraIcon_Button.bounds.height / 2
     profile_ImageView.layer.cornerRadius = profile_ImageView.bounds.height / 2
     profile_ImageView.layer.borderWidth = 2.0
     profile_ImageView.layer.borderColor = UIColor.white.cgColor
     profile_ImageView.clipsToBounds = true
     
+    
+    // Image action and handgesture
+    imageViewHandGesture()
+    
+    // UserID and Username
     let userDefaults = UserDefaults.standard
     self.userID = userDefaults.string(forKey: "uid") ?? ""
     self.username = userDefaults.string(forKey: "username") ?? ""
     
+    // See if there's username
     if username != "" {
       name_Label.text = self.username
     } else {
       name_Label.text = username
     }
+    
+    // fetch Data
     fetchData()
   }
   
@@ -60,84 +74,19 @@ class SettingViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
   override func viewWillAppear(_ animated: Bool) {
     if let index = self.tableView.indexPathForSelectedRow{
       self.tableView.deselectRow(at: index, animated: true)
     }
   }
   
-  @IBAction func cancelViewController(_ segue: UIStoryboardSegue) {
-  }
-  @IBAction func saveDetail(_ segue: UIStoryboardSegue) {
-    fetchData()
-  }
-  var user = User()
-  func fetchData(){
-    self.databaseRef = Database.database().reference()
-    self.databaseRef.child("Users/\(self.userID)").observe(.value) { (snapshot) in
-      if let dictionary = snapshot.value as? [String: AnyObject]{
-        print("dictionary is \(dictionary)")
-        
-        let user = User()
-        guard let name = dictionary["name"] else {
-          self.databaseRef.child("Users/\(self.userID)").child("name").setValue("")
-          return
-        }
-        guard let account = dictionary["account"] else {
-          self.databaseRef.child("Users/\(self.userID)").child("account").setValue("")
-          return
-        }
-        
-        guard let birthday = dictionary["birthday"] else {
-          self.databaseRef.child("Users/\(self.userID)").child("birthday").setValue("")
-          return
-        }
-        
-        guard let gender = dictionary["gender"] else {
-          self.databaseRef.child("Users/\(self.userID)").child("gender").setValue("")
-          return
-        }
-        
-        guard let profileImageUrl = dictionary["profileImageUrl"] else {
-          self.databaseRef.child("Users/\(self.userID)").child("profileImageUrl").setValue("")
-          return
-        }
-        
-        user.name = name as? String
-        user.account = account as? String
-        user.birthday = birthday as? String
-        user.gender = gender as? String
-        user.profileImageUrl = profileImageUrl as? String
-        
-        if user.name != ""{
-          self.name_Label.text = user.name
-        } else {
-          self.name_Label.text = ""
-        }
-
-        if user.profileImageUrl != ""{
-          let url = URL(string: user.profileImageUrl!)
-          self.profile_ImageView.kf.setImage(with: url)
-        }
-        
-        self.user = user
-      }
-      
-    }
-
-  }
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if segue.identifier == "identifierEdit"{
-      let destination = segue.destination as! SettingEditViewController
-      destination.user = self.user
-    }
-  }
-  
-  @IBOutlet weak var Edit_Button: UIBarButtonItem!
+  // MARK: Edit Button Tapped
   @IBAction func Edit_Button_Tapped(_ sender: UIBarButtonItem) {
     performSegue(withIdentifier: "identifierEdit", sender: self)
   }
   
+  // MARK: Log Out Button Tapped
   @IBAction func LogIn_LogOut(_ sender: UIButton) {
     FBSDKLoginManager().logOut()
     do{
@@ -155,11 +104,82 @@ class SettingViewController: UIViewController {
     }
     
   }
-  @IBAction func BackToSetting( segue:UIStoryboardSegue ) { }
   
   
   @IBAction func cameraOrImage(_ sender: UIButton) {
     camera()
+    cameraIcon_Button.isHighlighted = true
+  }
+  
+  
+  // MARK: Segue
+  @IBAction func BackToSetting( segue: UIStoryboardSegue ) { }
+
+  @IBAction func cancelViewController(_ segue: UIStoryboardSegue) {
+  }
+  @IBAction func saveDetail(_ segue: UIStoryboardSegue) {
+    fetchData()
+  }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "identifierEdit"{
+      let destination = segue.destination as! SettingEditViewController
+      destination.user = self.user
+    }
+  }
+  
+}
+
+// MARK: UITableViewDelegate, UITableViewDataSource
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource
+{
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return list.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SettingMainTableViewCell
+    cell.settingTitle_Label.text = list[indexPath.row]
+    return cell
+  }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    if indexPath.row == 0{
+//      performSegue(withIdentifier: "points", sender: self)
+//    } else
+      if indexPath.row == 0{
+      performSegue(withIdentifier: "lecturer", sender: self)
+    } else if indexPath.row == 1{
+      performSegue(withIdentifier: "suggestion", sender: self)
+    } //else if indexPath.row == 3{
+      //performSegue(withIdentifier: "personal", sender: self)
+    //}
+       else if indexPath.row == 2{
+      performSegue(withIdentifier: "coupon", sender: self)
+    } else if indexPath.row == 3{
+      performSegue(withIdentifier: "about", sender: self)
+    } else{
+      print("WTF happened!!!")
+    }
+  }
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 40
+  }
+  
+}
+
+// MARK: Camera Picker
+extension SettingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  func imageViewHandGesture(){
+    let tap =
+      UITapGestureRecognizer(
+        target:self,
+        action:#selector(camera(recognizer:)))
+    profile_ImageView.isUserInteractionEnabled = true
+    profile_ImageView.addGestureRecognizer(tap)
+  }
+  @objc func camera(recognizer: UITapGestureRecognizer) {
+    cameraOrImage(cameraIcon_Button)
   }
   
   func camera(){
@@ -211,51 +231,6 @@ class SettingViewController: UIViewController {
     present(imagePickerAlertController, animated: true, completion: nil)
     
   }
-  
-  
-  
-  
-  
-}
-extension SettingViewController: UITableViewDelegate, UITableViewDataSource
-{
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return list.count
-  }
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SettingMainTableViewCell
-    cell.settingTitle_Label.text = list[indexPath.row]
-    return cell
-  }
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//    if indexPath.row == 0{
-//      performSegue(withIdentifier: "points", sender: self)
-//    } else
-      if indexPath.row == 0{
-      performSegue(withIdentifier: "lecturer", sender: self)
-    } else if indexPath.row == 1{
-      performSegue(withIdentifier: "suggestion", sender: self)
-    } //else if indexPath.row == 3{
-      //performSegue(withIdentifier: "personal", sender: self)
-    //}
-       else if indexPath.row == 2{
-      performSegue(withIdentifier: "coupon", sender: self)
-    } else if indexPath.row == 3{
-      performSegue(withIdentifier: "about", sender: self)
-    } else{
-      print("WTF happened!!!")
-    }
-  }
-  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 40
-  }
-  
-}
-
-extension SettingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     
@@ -331,5 +306,63 @@ extension SettingViewController: UIImagePickerControllerDelegate, UINavigationCo
   }
   
   
+}
+
+// APIs
+extension SettingViewController {
+  func fetchData(){
+    self.databaseRef = Database.database().reference()
+    self.databaseRef.child("Users/\(self.userID)").observe(.value) { (snapshot) in
+      if let dictionary = snapshot.value as? [String: AnyObject]{
+        print("dictionary is \(dictionary)")
+        
+        let user = User()
+        guard let name = dictionary["name"] else {
+          self.databaseRef.child("Users/\(self.userID)").child("name").setValue("")
+          return
+        }
+        guard let account = dictionary["account"] else {
+          self.databaseRef.child("Users/\(self.userID)").child("account").setValue("")
+          return
+        }
+        
+        guard let birthday = dictionary["birthday"] else {
+          self.databaseRef.child("Users/\(self.userID)").child("birthday").setValue("")
+          return
+        }
+        
+        guard let gender = dictionary["gender"] else {
+          self.databaseRef.child("Users/\(self.userID)").child("gender").setValue("")
+          return
+        }
+        
+        guard let profileImageUrl = dictionary["profileImageUrl"] else {
+          self.databaseRef.child("Users/\(self.userID)").child("profileImageUrl").setValue("")
+          return
+        }
+        
+        user.name = name as? String
+        user.account = account as? String
+        user.birthday = birthday as? String
+        user.gender = gender as? String
+        user.profileImageUrl = profileImageUrl as? String
+        
+        if user.name != ""{
+          self.name_Label.text = user.name
+        } else {
+          self.name_Label.text = ""
+        }
+        
+        if user.profileImageUrl != ""{
+          let url = URL(string: user.profileImageUrl!)
+          self.profile_ImageView.kf.setImage(with: url)
+        }
+        
+        self.user = user
+      }
+      
+    }
+    
+  }
 }
 
