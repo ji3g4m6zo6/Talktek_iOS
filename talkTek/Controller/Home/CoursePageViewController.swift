fda4078d96577470ea07e965a9d048d273f1e50e
sunnyleeyun <sunnylee945@hotmail.com>
2018-03-28 23:02:04 +0800

    course page add files

---
+++ talkTek/Controller/Home/CoursePageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CoursePageViewController.swift
+//  talkTek
+//
+//  Created by 李昀 on 2018/3/28.
+//  Copyright © 2018年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class CoursePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ talkTek/View/Home/CoursePage/CoursePageHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CoursePageHeaderTableViewCell.swift
+//  talkTek
+//
+//  Created by 李昀 on 2018/3/28.
+//  Copyright © 2018年 Talktek Inc. All rights reserved.
+//
+
+import UIKit
+
+class CoursePageHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
