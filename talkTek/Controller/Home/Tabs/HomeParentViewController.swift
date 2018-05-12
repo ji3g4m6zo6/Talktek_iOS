b430c0e9eac4834b70f1fdb6515a181da22cf39e
sunnyleeyun <sunnylee945@hotmail.com>
2018-05-13 00:30:27 +0800

    black underline

--- talkTek/Controller/Home/Tabs/HomeParentViewController.swift
+++ talkTek/Controller/Home/Tabs/HomeParentViewController.swift
@@ -19,6 +19,23 @@ class HomeParentViewController: ButtonBarPagerTabStripViewController {
 
   let tealish = UIColor.tealish()
   override func viewDidLoad() {
+    
+    settings.style.buttonBarBackgroundColor = .white
+    settings.style.buttonBarItemBackgroundColor = .white
+    settings.style.selectedBarBackgroundColor = UIColor.tealish()
+    settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+    settings.style.selectedBarHeight = 1.0
+    settings.style.buttonBarMinimumLineSpacing = 0
+    settings.style.buttonBarItemTitleColor = UIColor.tealish()
+    settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+    settings.style.buttonBarLeftContentInset = 0
+    settings.style.buttonBarRightContentInset = 0
+    changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+      guard changeCurrentIndex == true else { return }
+      oldCell?.label.textColor = UIColor.lightGray
+      newCell?.label.textColor = self?.tealish
+    }
+    
     super.viewDidLoad()
     
 //    listenToState()
@@ -31,21 +48,7 @@ class HomeParentViewController: ButtonBarPagerTabStripViewController {
     
     print(UserDefaults.standard.string(forKey: "uid") ?? "wtfffff???")
     
-    settings.style.buttonBarBackgroundColor = .white
-    settings.style.buttonBarItemBackgroundColor = .white
-    settings.style.selectedBarBackgroundColor = UIColor.tealish()
-    settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
-    settings.style.selectedBarHeight = CGFloat(1.0)
-    settings.style.buttonBarMinimumLineSpacing = 0
-    settings.style.buttonBarItemTitleColor = UIColor.tealish()
-    settings.style.buttonBarItemsShouldFillAvailiableWidth = true
-    settings.style.buttonBarLeftContentInset = 0
-    settings.style.buttonBarRightContentInset = 0
-    changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
-      guard changeCurrentIndex == true else { return }
-      oldCell?.label.textColor = UIColor.lightGray
-      newCell?.label.textColor = self?.tealish
-    }
+    
     
     
     
